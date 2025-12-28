5bea037b879bbfade9878ec568afbbdf883ce99b
Rob Marscher <rob@robmarscher.com>
2025-12-27 23:04:23 -0500

    platform-entry was removed

--- examples/07_cloudflare/src/main.ts
+++ examples/07_cloudflare/src/main.ts
@@ -1,9 +1,9 @@
 // @ts-expect-error ignore type here for now
 import serverEntry from 'virtual:vite-rsc-waku/server-entry'; // eslint-disable-line import/no-unresolved
-import handlers from "./platform-entry";
-export * from "./platform-entry";
+// import handlers from "./platform-entry";
+// export * from "./platform-entry";
 
 export default {
-  ...(handlers ? handlers : {}),
+  ...serverEntry.handlers,
   fetch: serverEntry.fetch,
-}
+};
